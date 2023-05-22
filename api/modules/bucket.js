ab6bbcb1251f1e618faaab33e107766e14318764
programist78 <pavelmolodecfda@gmail.com>
2023-05-22 22:38:28 +0300

    add

--- api/modules/bucket.js
+++ api/modules/bucket.js
@@ -1,15 +1,3 @@
-// import { S3 } from "@aws-sdk/client-s3";
-
-// export const bucket = "portfolio-webxwizard";
-
-// export const s3 = new S3({
-//   endpoint: "http://localhost:4000",
-//   accessKeyId: "AKIAQAK2Y3V6NHB7TV5F",
-//   secretAccessKey: "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",
-//   sslEnabled: false,
-//   s3ForcePathStyle: true,
-// });
-// region: "eu-central-1",
 import { S3Client } from "@aws-sdk/client-s3";
 
 export const bucket = process.env.BUCKET_NAME;
